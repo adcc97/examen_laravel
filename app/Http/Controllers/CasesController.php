2466abf45ccc0d5cbbfae09174b7595f5a572c44
DavidCastillo <adcc.97@outlook.com>
2021-09-04 12:31:11 -0500

    Creacion de crud para clientes

---
+++ app/Http/Controllers/CasesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Cases;
+use Illuminate\Http\Request;
+
+class CasesController extends Controller
+{
+    public function index(){
+
+        $cases = Cases::latest()->get();
+        return view('cases.index',[
+            'cases' => $cases
+        ]);
+
+    }
+
+    public function store(Request $request){
+
+        Cases::create([
+            'name' => $request->name,
+            'typeofcase' => $request->typeofcase,
+            'stateOfCase' => $request->stateOfCase
+        ]);
+
+    }
+
+    public function destroy(Cases $cases){
+
+        $cases->delete();
+        return back();
+
+    }
+
+    }
+
